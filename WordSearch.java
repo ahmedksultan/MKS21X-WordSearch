66f2c024752a2eef55dcb308e4450e95b82430b9
asultan00 <asultan00@stuy.edu>
2018-11-18 14:28:22 -0500

    created second constructor (w/ fileName), and verified that the scanner works properly

--- WordSearch.java
+++ WordSearch.java
@@ -11,6 +11,17 @@ public class WordSearch {
                     System.out.println(output);
                     System.exit(1);
                }
+               if (args.length == 3) {
+                    int row = Integer.parseInt(args[0]);
+                    int col = Integer.parseInt(args[1]);
+                    WordSearch output = new WordSearch(row, col, args[2]);
+                    /*
+                    System.out.println(output);
+                    System.out.println(output.wordsToAdd);
+                    System.out.println(output.randgen);
+                    */
+                    System.exit(1);
+               }
           }
           catch (NumberFormatException e) {
                System.out.println("Error!");
@@ -37,6 +48,34 @@ public class WordSearch {
           }
      }
 
+     //CONSTRUCTOR 2 - rows, class, fileName (3args)
+     public WordSearch(int rows, int cols, String fileName) {
+          if (rows < 0 || cols < 0) {
+               throw new IllegalArgumentException("Given dimensions are out of bounds!");
+          }
+          wordsToAdd = new ArrayList<String>();
+          wordsAdded = new ArrayList<String>();
+          try {
+               File words = new File(fileName);
+               Scanner in = new Scanner(words);
+               while(in.hasNext()) {
+                    wordsToAdd.add(in.nextLine());
+               }
+          }
+          catch (FileNotFoundException e) {
+               System.out.println("File not found: " + fileName);
+               System.exit(1);
+          }
+          data = new char[rows][cols];
+          for (int a = 0; a < data.length; a++) {
+               for (int b = 0; b < data[a].length; b++) {
+                    data[a][b] = '_';
+               }
+          }
+          randgen = new Random();
+          //seed = ???
+     }
+
      public String toString() {
           String result = "";
           for (int a = 0; a < data.length; a++) {
@@ -50,5 +89,61 @@ public class WordSearch {
           }
           return result + "\nWords: " + wordsAdded + "\n";
      }
-     
+
+
+
+
+
+
+
+
+
+
+
+     //LEGACY FUNCTIONS (addHorizontal/addVertical/addDiagonal)
+     public boolean addWordHorizontal(String word,int row, int col) {
+         for (int i = 0; i < word.length(); i++) {
+              /* ifstatement is checking to make sure...
+                   > word isn't too big;
+                   > letters of word can overlap
+                   > current spot is empty
+              */
+              if (col + i >= data[row].length ||
+                   ((word.charAt(i) != data[row][col+i]) &&
+                   (data[row][col+i] != '_'))) {
+                        return false;
+              }
+         }
+         for (int i = 0; i < word.length(); i++) {
+              data[row][col+i] = word.charAt(i);
+         }
+         return true;
+     }
+     public boolean addWordVertical(String word, int row, int col) {
+          for (int i = 0; i < word.length(); i++) {
+               if (row + i >= data.length ||
+                    ((word.charAt(i) != data[row+i][col]) &&
+                    (data[row+i][col] != '_'))) {
+                         return false;
+               }
+          }
+          for (int i = 0; i < word.length(); i++) {
+               data[row+i][col] = word.charAt(i);
+          }
+          return true;
+     }
+     public boolean addWordDiagonal(String word, int row, int col) {
+          for (int i = 0; i < word.length(); i++) {
+               if (row + i >= data.length ||
+                    col + i >= data[row].length ||
+                    ((word.charAt(i) != data[row+i][col+i]) &&
+                    (data[row+i][col+i] != '_'))) {
+                         return false;
+               }
+          }
+          for (int i = 0; i < word.length(); i++) {
+               data[row+i][col+i] = word.charAt(i);
+          }
+          return true;
+     }
 }
